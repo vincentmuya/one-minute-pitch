b698dcda6e190cb6427f3a335ffbe9b930a12d84
vincentmuya <vincentmuya13@gmail.com>
2018-02-04 00:42:33 +0300

    modified code in init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,6 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
-from config import config_options
 
-bootstrap = Bootstrap()
+# Initializing application
+app = Flask(__name__)
 
-def create_app(config_name):
-
-    #initializing application
-    app = Flask(__name__)
-
-    #creating the app configarations
-    app.config.from_object(config_options[config_name])
-
-    #initializing flask extensions
-    bootstrap.init_app(app)
-return app
+from app import views
