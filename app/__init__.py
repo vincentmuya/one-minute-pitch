12e50079f5e8f0239bd15f823afad29f1536034b
vincentmuya <vincentmuya13@gmail.com>
2018-02-05 03:48:08 +0300

    modified code in init

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 # Initializing application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
 #setting up configuration
 app.config.from_object(DevConfig)
