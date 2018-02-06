eccf6a0df2c556884afbd63a7d7bc04f88e37373
vincentmuya <vincentmuya13@gmail.com>
2018-02-06 04:35:43 +0300

    created manay to many relationship

--- app/models.py
+++ app/models.py
@@ -13,18 +13,24 @@ class Pitches:
         self.vote_count = vote_count
 
 class User(db.Model):
-    __tablename__='users'
-    id =db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(225))
-
-    def __repr__(self):
-        return f'User{self.username}'
-
-class Role(db.Model):
-    __table__='roles'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(225))
+    username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
 
     def __repr__(self):
-        return f'User{self.name}'
+        return f'User {self.username}'
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
