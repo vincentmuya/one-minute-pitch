f6b8e0c24a2d6e321d0c8ee8fb711dfabae5c981
vincentmuya <vincentmuya13@gmail.com>
2018-02-06 04:23:00 +0300

    created role table

--- app/models.py
+++ app/models.py
@@ -19,3 +19,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User{self.username}'
+
+class Role(db.Model):
+    __table__='roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(225))
+
+    def __repr__(self):
+        return f'User{self.name}'
