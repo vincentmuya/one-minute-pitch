c275566193b0090733b40fd4d2541a3dc8f8a1dc
vincentmuya <vincentmuya13@gmail.com>
2018-02-08 13:58:12 +0300

    created email welcome message

--- app/auth/views.py
+++ app/auth/views.py
@@ -37,6 +37,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
         title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
+        return render_template('auth/register.html',registration_form = form)
